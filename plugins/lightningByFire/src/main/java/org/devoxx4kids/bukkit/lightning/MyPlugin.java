b6a91841dcf10ea0da9ca49d17f88e708a429867
Aditya Gupta <adityag@gmail.com>
2014-01-26 11:07:08 -0800

    Event handler: When an entity dies by fire, lightning will strike
    Starting to work on my first mod for the bukkit modding workshop.

---
+++ plugins/lightningByFire/src/main/java/org/devoxx4kids/bukkit/lightning/MyPlugin.java
@@ -0,0 +1,23 @@
+package org.devoxx4kids.bukkit.lightning;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MyPlugin extends JavaPlugin {
+    // This code is called only once after the server starts
+    @Override
+    public void onLoad() {
+        getLogger().info("MyPlugin.onLoad()");
+    }
+
+    // This code is called after the server starts and after the /reload command
+    @Override
+    public void onEnable() {
+        getLogger().info("MyPlugin.onEnable()");
+    }
+
+    // This code is called before the server stops and after the /reload command
+    @Override
+    public void onDisable() {
+        getLogger().info("MyPlugin.onDisable()");
+    }
+}
