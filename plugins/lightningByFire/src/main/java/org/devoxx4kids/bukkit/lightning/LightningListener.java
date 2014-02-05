1401d6f568b0107c390ad36ffc5497fdf7335d8d
Aditya Gupta <adityag@gmail.com>
2014-02-04 17:43:59 -0800

    Forgot to add the listener, so adding it now.

---
+++ plugins/lightningByFire/src/main/java/org/devoxx4kids/bukkit/lightning/LightningListener.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright: Aditya Gupta
+ */
+package org.devoxx4kids.bukkit.lightning;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.EntityDeathEvent;
+
+/**
+ * @author Aditya Gupta
+ */
+public class LightningListener implements Listener{
+    
+    @EventHandler
+    public void lightningByFire(EntityDeathEvent event){
+            Entity entity = event.getEntity();
+            Location location = entity.getLocation();
+            World world = entity.getWorld();
+            EntityDamageEvent x = entity.getLastDamageCause();
+            
+            if(x.getCause() == DamageCause.LAVA || x.getCause() == DamageCause.FIRE || x.getCause() == DamageCause.FIRE_TICK){
+                world.strikeLightningEffect(location);
+            }
+    }
+}
