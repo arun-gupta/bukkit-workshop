35efc524f5952e8e0c32023d6493590c2c29c00c
Aditya Gupta <adityag@gmail.com>
2014-02-04 17:37:29 -0800

    Made a plugin to add a new crafting recipe: 4 diamonds, 2 emeralds, 2 blocks of lapis lazuli, and a nether star make a potato.

---
+++ plugins/craftingRecipe/src/main/java/org/devoxx4kids/bukkit/crafting/CraftingListener.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright: Aditya Gupta
+ */
+package org.devoxx4kids.bukkit.crafting;
+
+import org.bukkit.event.Listener;
+
+/**
+ * @author Aditya Gupta
+ */
+public class CraftingListener implements Listener { 
+    
+}
---
+++ plugins/craftingRecipe/src/main/java/org/devoxx4kids/bukkit/crafting/MyPlugin.java
@@ -0,0 +1,29 @@
+package org.devoxx4kids.bukkit.crafting;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MyPlugin extends JavaPlugin {
+    // This code is called only once after the server starts
+    @Override
+    public void onLoad() {
+        getLogger().info("MyPlugin.onLoad()");
+    }
+
+    // This code is called after the server starts and after the /reload command
+    @Override
+    public void onEnable() {
+        getLogger().info("MyPlugin.onEnable()");
+        getServer().getPluginManager().registerEvents(new CraftingListener(), this);
+        ShapedRecipe recipe = new ShapedRecipe(new ItemStack(Material.POTATO_ITEM, 1)).shape("ded", "lnl", "ded").setIngredient('d', Material.DIAMOND).setIngredient('e', Material.EMERALD).setIngredient('l', Material.LAPIS_BLOCK).setIngredient('n', Material.NETHER_STAR);
+        getServer().addRecipe(recipe);
+    }
+
+    // This code is called before the server stops and after the /reload command
+    @Override
+    public void onDisable() {
+        getLogger().info("MyPlugin.onDisable()");
+    }
+}
