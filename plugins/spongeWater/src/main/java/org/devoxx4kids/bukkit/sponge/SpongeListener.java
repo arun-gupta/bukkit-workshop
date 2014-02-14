9b0ef7bc18bd90ffd68d6869ba058a521b248ad6
Aditya Gupta <adityag@gmail.com>
2014-02-13 19:23:09 -0800

    Made a plugin that makes it so when you place a sponge and it is powered, it will destroy all water directly or diagonally adjacent to it.

---
+++ plugins/spongeWater/src/main/java/org/devoxx4kids/bukkit/sponge/SpongeListener.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright: Aditya Gupta
+ */
+package org.devoxx4kids.bukkit.sponge;
+
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+/**
+ * @author Aditya Gupta
+ */
+public class SpongeListener implements Listener {
+
+    @EventHandler
+    public void absorbWater(BlockPlaceEvent event) {
+        Block eventblock = event.getBlock();
+        World world = eventblock.getWorld();
+        if(eventblock.getType() == Material.SPONGE && eventblock.isBlockPowered()){
+            int x = eventblock.getX();
+            int y = eventblock.getY();
+            int z = eventblock.getZ();
+            
+            int[] areaAroundSponge = {
+                x, y + 1, z,
+                x, y - 1, z,
+                
+                x + 1, y + 1, z,
+                x - 1, y + 1, z,
+                x, y + 1, z + 1,
+                x, y + 1, z - 1,
+                x + 1, y + 1, z + 1,
+                x - 1, y + 1, z - 1,
+                x + 1, y + 1, z - 1,
+                x - 1, y + 1, z + 1,
+                    
+                x + 1, y, z,
+                x - 1, y, z,
+                x, y, z + 1,
+                x, y, z - 1,
+                x + 1, y, z + 1,
+                x - 1, y, z - 1,
+                x + 1, y, z - 1,
+                x - 1, y, z + 1,
+                        
+                x + 1, y + 2, z,
+                x - 1, y + 2, z,
+                x, y + 2, z + 1,
+                x, y + 2, z - 1,
+                x + 1, y + 2, z + 1,
+                x - 1, y + 2, z - 1,
+                x + 1, y + 2, z - 1,
+                x - 1, y + 2, z + 1
+            };
+
+            for (int i = 0; i < areaAroundSponge.length; i = i + 3) {
+                Block block = world.getBlockAt(areaAroundSponge[i], areaAroundSponge[i + 1], areaAroundSponge[i + 2]);
+                if(block.getType() == Material.WATER){
+                    block.setType(Material.AIR);
+                }
+            }
+        }
+    }
+}
