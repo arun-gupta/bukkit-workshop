b024eb7a2b34099cc4f9248c7d7a40a519d049cb
Aditya Gupta <adityag@gmail.com>
2014-02-13 19:56:48 -0800

    Changed it so the sponge works when not powered and makes obsidian instead of air.

--- plugins/spongeWater/src/main/java/org/devoxx4kids/bukkit/sponge/SpongeListener.java
+++ plugins/spongeWater/src/main/java/org/devoxx4kids/bukkit/sponge/SpongeListener.java
@@ -19,15 +19,15 @@ public class SpongeListener implements Listener {
     public void absorbWater(BlockPlaceEvent event) {
         Block eventblock = event.getBlock();
         World world = eventblock.getWorld();
-        if(eventblock.getType() == Material.SPONGE && eventblock.isBlockPowered()){
+        if (eventblock.getType() == Material.SPONGE) {
+
             int x = eventblock.getX();
             int y = eventblock.getY();
             int z = eventblock.getZ();
-            
+
             int[] areaAroundSponge = {
                 x, y + 1, z,
                 x, y - 1, z,
-                
                 x + 1, y + 1, z,
                 x - 1, y + 1, z,
                 x, y + 1, z + 1,
@@ -36,7 +36,6 @@ public class SpongeListener implements Listener {
                 x - 1, y + 1, z - 1,
                 x + 1, y + 1, z - 1,
                 x - 1, y + 1, z + 1,
-                    
                 x + 1, y, z,
                 x - 1, y, z,
                 x, y, z + 1,
@@ -45,21 +44,20 @@ public class SpongeListener implements Listener {
                 x - 1, y, z - 1,
                 x + 1, y, z - 1,
                 x - 1, y, z + 1,
-                        
-                x + 1, y + 2, z,
-                x - 1, y + 2, z,
-                x, y + 2, z + 1,
-                x, y + 2, z - 1,
-                x + 1, y + 2, z + 1,
-                x - 1, y + 2, z - 1,
-                x + 1, y + 2, z - 1,
-                x - 1, y + 2, z + 1
+                x + 1, y - 1, z,
+                x - 1, y - 1, z,
+                x, y - 1, z + 1,
+                x, y - 1, z - 1,
+                x + 1, y - 1, z + 1,
+                x - 1, y - 1, z - 1,
+                x + 1, y - 1, z - 1,
+                x - 1, y - 1, z + 1
             };
 
             for (int i = 0; i < areaAroundSponge.length; i = i + 3) {
                 Block block = world.getBlockAt(areaAroundSponge[i], areaAroundSponge[i + 1], areaAroundSponge[i + 2]);
-                if(block.getType() == Material.WATER){
-                    block.setType(Material.AIR);
+                if (block.getType() == Material.WATER) {
+                    block.setType(Material.OBSIDIAN);
                 }
             }
         }
