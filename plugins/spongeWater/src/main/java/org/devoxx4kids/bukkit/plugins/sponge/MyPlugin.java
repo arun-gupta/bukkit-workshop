1b29ca167dbb0462020f442a6c5fda9fa5836993
Ryan Michela <deltahat@gmail.com>
2014-02-23 19:53:28 -0800

    Fixed sponge plugin. Resolves #1

--- plugins/spongeWater/src/main/java/org/devoxx4kids/bukkit/plugins/sponge/MyPlugin.java
+++ plugins/spongeWater/src/main/java/org/devoxx4kids/bukkit/plugins/sponge/MyPlugin.java
@@ -1,7 +1,8 @@
 package org.devoxx4kids.bukkit.plugins.sponge;
 
-import java.util.logging.Level;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
 
 public class MyPlugin extends JavaPlugin {
     // This code is called only once after the server starts
@@ -14,7 +15,7 @@ public class MyPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().log(Level.INFO, "{0}.onEnable()", this.getClass().getName());
-        getServer().getPluginManager().registerEvents(new SpongeListener(), this);
+        getServer().getPluginManager().registerEvents(new SpongeListener(this), this);
     }
 
     // This code is called before the server stops and after the /reload command
--- plugins/spongeWater/src/main/java/org/devoxx4kids/bukkit/plugins/sponge/SpongeListener.java
+++ plugins/spongeWater/src/main/java/org/devoxx4kids/bukkit/plugins/sponge/SpongeListener.java
@@ -9,57 +9,43 @@ import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.plugin.Plugin;
 
 /**
  * @author Aditya Gupta
  */
 public class SpongeListener implements Listener {
 
+    private Plugin plugin;
+
+    public SpongeListener(Plugin plugin) {
+        this.plugin = plugin;
+    }
+
     @EventHandler
     public void absorbWater(BlockPlaceEvent event) {
         Block eventblock = event.getBlock();
         World world = eventblock.getWorld();
         if (eventblock.getType() == Material.SPONGE) {
+            plugin.getLogger().info("Sponge place detected.");
 
             int x = eventblock.getX();
             int y = eventblock.getY();
             int z = eventblock.getZ();
 
-            int[] areaAroundSponge = {
-                x, y + 1, z,
-                x, y - 1, z,
-                x + 1, y + 1, z,
-                x - 1, y + 1, z,
-                x, y + 1, z + 1,
-                x, y + 1, z - 1,
-                x + 1, y + 1, z + 1,
-                x - 1, y + 1, z - 1,
-                x + 1, y + 1, z - 1,
-                x - 1, y + 1, z + 1,
-                x + 1, y, z,
-                x - 1, y, z,
-                x, y, z + 1,
-                x, y, z - 1,
-                x + 1, y, z + 1,
-                x - 1, y, z - 1,
-                x + 1, y, z - 1,
-                x - 1, y, z + 1,
-                x + 1, y - 1, z,
-                x - 1, y - 1, z,
-                x, y - 1, z + 1,
-                x, y - 1, z - 1,
-                x + 1, y - 1, z + 1,
-                x - 1, y - 1, z - 1,
-                x + 1, y - 1, z - 1,
-                x - 1, y - 1, z + 1
-            };
-
-            for (int i = 0; i < areaAroundSponge.length; i = i + 3) {
-                Block block = world.getBlockAt(areaAroundSponge[i], areaAroundSponge[i + 1], areaAroundSponge[i + 2]);
-                if (block.getType() == Material.WATER) {
-                    block.setType(Material.OBSIDIAN);
+            int waterBlocksFound = 0;
+            for (int xx = x - 1; xx <= x + 1; xx++) {
+                for (int yy = y - 1; yy <= y + 1; yy++) {
+                    for (int zz = z - 1; zz <= z + 1; zz++) {
+                        Block block = world.getBlockAt(xx, yy, zz);
+                        if (block.getType() == Material.WATER || block.getType() == Material.STATIONARY_WATER) {
+                            block.setType(Material.OBSIDIAN);
+                            waterBlocksFound++;
+                        }
+                    }
                 }
             }
+            plugin.getLogger().info("Replaced " + waterBlocksFound + " water blocks with obsidian.");
         }
     }
 }
