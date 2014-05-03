397fe8a22784b25c9ab778a9c79fa5041ce15994
Aditya Gupta <adityag@gmail.com>
2014-05-03 15:42:24 -0700

    Added a plugin to make bigger TNT explosions.

---
+++ plugins/BiggerTNTExplosions/src/main/java/org/aditya/mods/bukkit/biggertnt/MyPlugin.java
@@ -0,0 +1,25 @@
+package org.aditya.mods.bukkit.biggertnt;
+
+import java.util.logging.Level;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MyPlugin extends JavaPlugin {
+    // This code is called only once after the server starts
+    @Override
+    public void onLoad() {
+        getLogger().log(Level.INFO, "{0}.onLoad()", this.getClass().getName());
+    }
+
+    // This code is called after the server starts and after the /reload command
+    @Override
+    public void onEnable() {
+        getLogger().log(Level.INFO, "{0}.onEnable()", this.getClass().getName());
+        getServer().getPluginManager().registerEvents(new TNTListener(), this);
+    }
+
+    // This code is called before the server stops and after the /reload command
+    @Override
+    public void onDisable() {
+        getLogger().log(Level.INFO, "{0}.onDisable()", this.getClass().getName());
+    }
+}
---
+++ plugins/BiggerTNTExplosions/src/main/java/org/aditya/mods/bukkit/biggertnt/TNTListener.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright: Aditya Gupta
+ */
+package org.aditya.mods.bukkit.biggertnt;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
+
+/**
+ * @author Aditya Gupta
+ */
+public class TNTListener implements Listener {
+
+    @EventHandler
+    public void makeExplosion(EntityExplodeEvent event) {
+        EntityType entitytype = event.getEntityType();
+        World world = event.getEntity().getWorld();
+        Location location = event.getEntity().getLocation();
+
+        float power = 20.0F;
+        if (entitytype == EntityType.PRIMED_TNT) {
+            event.setCancelled(true);
+            world.createExplosion(location, power);
+        }
+    }
+}
