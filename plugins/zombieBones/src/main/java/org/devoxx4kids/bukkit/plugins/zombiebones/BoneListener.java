bac34a62981842c1ce2bc8abe646c0b3eada34ad
Aditya Gupta <adityag@gmail.com>
2014-05-26 12:05:38 -0700

    Added a Listener,

---
+++ plugins/zombieBones/src/main/java/org/devoxx4kids/bukkit/plugins/zombiebones/BoneListener.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright: Aditya Gupta
+ */
+package org.devoxx4kids.bukkit.plugins.zombiebones;
+
+import java.util.Random;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * @author Aditya Gupta
+ */
+class BoneListener implements Listener {
+
+    public BoneListener() {
+    }
+
+    @EventHandler
+    public void dropBones(EntityDeathEvent e) {
+        if(e.getEntityType() != EntityType.ZOMBIE){
+            return;
+        }
+        Location l = e.getEntity().getLocation();
+        World w = e.getEntity().getWorld();
+        
+        Random r = new Random();
+        int i = r.nextInt(2);
+        
+        w.dropItem(l, new ItemStack(Material.BONE, i));
+    }
+}
